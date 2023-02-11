4183d61568a50abf46d2add8a8ee183bf8f8f2e2
wangyiwen <yiwenw315@gmail.com>
2023-02-10 22:53:57 -0800

    Docker and K8s Learning

---
+++ M1-Python-based-docker/rng.py
@@ -0,0 +1,7 @@
+from random import randint
+
+min_number = int(input('Please input the min number:'))
+max_number = int(input('Please input the max number:'))
+
+rnd = randint(min_number,max_number)
+print('The rand intger is:',rnd)
